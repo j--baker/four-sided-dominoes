d22091a10f64bdf14214e5da1f195b39727e3ab0
Jonathan Baker <jonathan_baker@rpmpcg.com>
2022-01-22 19:01:30 -0500

    Broken Gulp config, updated Node stuff

--- gulpfile.js
+++ gulpfile.js
@@ -10,4 +10,11 @@ function transpile_node() {
 		.pipe( gulp.dest( 'target/node' ) );
 }
 
+//function transpile_public_es6() {
+//	return gulp.src( 'src/public/es6/**/*.js' )
+//		.pipe( babel( { presets: [ '@babel/preset-env' ] } ) )
+//		.pipe( gulp.dest( 'target/public' ) );
+//}
+
+//exports.default = gulp.serial( transpile_node /*, tranpile_public_es6 */ );
 exports.default = transpile_node;
